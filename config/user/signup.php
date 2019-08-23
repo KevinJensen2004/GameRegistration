99ae114546f0235881cdec8c8cc92234679f9bcb
KevinJensen2004 <51212703+KevinJensen2004@users.noreply.github.com>
2019-08-23 15:29:14 +0200

    Update signup.php

--- config/user/signup.php
+++ config/user/signup.php
@@ -1,1 +1,28 @@
-
+// signup user
+function signup(){
+    if($this->isAlreadyExist()){
+        return false;
+    }
+    // query to insert record
+    $query = "INSERT INTO
+                " . $this->table_name . "
+            SET
+                username=:username, password=:password, created=:created";
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+    // sanitize
+    $this->username=htmlspecialchars(strip_tags($this->username));
+    $this->password=htmlspecialchars(strip_tags($this->password));
+    $this->created=htmlspecialchars(strip_tags($this->created));
+    // bind values
+    $stmt->bindParam(":username", $this->username);
+    $stmt->bindParam(":password", $this->password);
+    $stmt->bindParam(":created", $this->created);
+    // execute query
+    if($stmt->execute()){
+        $this->id = $this->conn->lastInsertId();
+        return true;
+    }
+    return false;
+    
+}
