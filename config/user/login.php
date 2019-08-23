9a9b815ecfc7d3546606258b9cdb41efa51e86fc
KevinJensen2004 <51212703+KevinJensen2004@users.noreply.github.com>
2019-08-23 15:30:03 +0200

    Update login.php

--- config/user/login.php
+++ config/user/login.php
@@ -1,1 +1,16 @@
+function login(){
+    // select all query
+    $query = "SELECT
+                `id`, `username`, `password`, `created`
+            FROM
+                " . $this->table_name . " 
+            WHERE
+                username='".$this->username."' AND password='".$this->password."'";
 
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+
+    // execute query
+    $stmt->execute();
+    return $stmt;
+}
